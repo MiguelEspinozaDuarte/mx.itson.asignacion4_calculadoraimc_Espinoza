a26592ff03af6e5079db8f3bfb559a48db0de18e
USER-PC\mj_es <mj_espinoza30@hotmail.com>
2023-02-06 00:16:53 -0600

    Initial commit

---
+++ app/src/test/java/com/example/mxitsonasignacion4_calculadoraimc_espinoza/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.mxitsonasignacion4_calculadoraimc_espinoza
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
